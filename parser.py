83ba9268870b6aad1be836bb61132cfeb580fcb4
mr-cloud <zhangxin1992pm@gmail.com>
2017-12-14 21:58:26 +0800

    Developed parser.

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 import json
 
 
-data_dir = 'test'
+data_dir = 'datasets.test'
 schema = 'username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink'
 format = '%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s'
 BEFORE = ';"'
@@ -12,9 +12,11 @@ DELIMITER = ';'
 POS_THRESHOLD = 0.3
 NEG_THRESHOLD = -0.3
 date_format = '%Y-%m-%d %H:%M'
-DATE_DEL = ' '
+DATE_DEL = '-'
 rst = {}
-rst_file = 'rst.json'
+TOTAL_FIELD = 'total'
+hashtag = 'TimesSquare'
+rst_file = hashtag + '-' + 'rst.json'
 with open(rst_file, 'w') as output:
     for file in os.listdir(data_dir):
         print('processing {}'.format(file))
@@ -24,20 +26,23 @@ with open(rst_file, 'w') as output:
                 if line_num == 0:
                     line_num += 1
                     continue
+                line = line.decode('utf-8')
                 try:
                     before_date = line.index(DELIMITER)
-                    after_date = line.index(DELIMITER, before_text + len(DELIMITER))
-                    dateStr = line[before_text + len(DELIMITER), after_text]
+                    after_date = line.index(DELIMITER, before_date + len(DELIMITER))
+                    dateStr = line[before_date + len(DELIMITER): after_date]
                     dateStr = dateStr.split(DATE_DEL)[0]
                     before_text = line.index(BEFORE) + len(BEFORE) - 1
                     after_text = line.index(AFTER, before_text + 1)
-                    text = line[before_text + 1, after_text]
+                    text = line[before_text + 1: after_text]
                     after_geo = line.index(DELIMITER, after_text + len(AFTER))
-                    geoStr = line[after_text + len(AFTER), after_geo]
+                    geoStr = line[after_text + len(AFTER): after_geo]
                 except ValueError as ve:
                     print(ve)
                     continue
+
                 polarity = TextBlob(text).sentiment.polarity
+
                 geo_sup = 0
                 geo_oppo = 0
                 geo_vot = 0
@@ -45,15 +50,18 @@ with open(rst_file, 'w') as output:
                 total_oppo = 0
                 total_vot = 0
 
-                time = rst[dateStr]
-                if rst[dateStr] is None:
-                    time = {}
-                geo = time[geoStr]
-                if time[geoStr] is None:
-                    geo = []
-                total = time['all']
-                if time['all']:
-                    total = []
+                time = {}
+                if rst.has_key(dateStr):
+                    time = rst[dateStr]
+
+                geo = []
+                if time.has_key(geoStr):
+                    geo = time[geoStr]
+
+                total = []
+                if time.has_key(TOTAL_FIELD):
+                    total = time[TOTAL_FIELD]
+
                 if len(geo) != 0:
                     geo_sup = geo[0]
                     geo_oppo = geo[1]
@@ -78,7 +86,7 @@ with open(rst_file, 'w') as output:
                     geo = [geo_sup, geo_oppo, geo_vot]
                     time[geoStr] = geo
                 total = [total_sup, total_oppo, total_vot]
-                time['all'] = total
+                time[TOTAL_FIELD] = total
                 rst[dateStr] = time
     rstStr = json.dumps(rst)
     output.write(rstStr)
